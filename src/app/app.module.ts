950fffc3f80e0f24dc92297979e4b1622431c51a
larin.kirill4 <larin.kirill4@wb.ru>
2023-07-12 13:54:47 +0300

    icons module and base layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,36 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MainLayoutComponent } from './components/main-layout/main-layout.component';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
-import { environment } from '../environments/environment';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
-import { provideStorage,getStorage } from '@angular/fire/storage';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {MainLayoutComponent} from './components/main-layout/main-layout.component';
+import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import {environment} from '../environments/environment';
+import {provideAuth, getAuth} from '@angular/fire/auth';
+import {provideFirestore, getFirestore} from '@angular/fire/firestore';
+import {provideStorage, getStorage} from '@angular/fire/storage';
+import {SidebarComponent} from './components/sidebar/sidebar.component';
+import {IconModule} from "@ant-design/icons-angular";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainLayoutComponent
+    MainLayoutComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
-    provideStorage(() => getStorage())
+    provideStorage(() => getStorage()),
+    IconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'aame-1da5f',
+    appId: '1:965217255129:web:c8856a5e9baa67530648c8',
+    storageBucket: 'aame-1da5f.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyDk0fR4YXRhV3AHSkszWOHh5dz0s49XRPc',
+    authDomain: 'aame-1da5f.firebaseapp.com',
+    messagingSenderId: '965217255129',
+  },
+};
